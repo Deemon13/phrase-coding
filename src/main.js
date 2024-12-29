dec174df64a8cb66af520dc9db51daf1e545ccd7
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2024-12-29 21:53:51 +0300

    fix input

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
-import "./style.css";
+import './style.css';
 
-const app = document.getElementById("app");
+const app = document.getElementById('app');
 
 app.innerHTML = `
 <div class='container'>
@@ -12,67 +12,68 @@ app.innerHTML = `
 <div><button class='button button-bufer' type='button' id='bufer' disabled>Скопировать в буфер обмена</button></div>
 </div>`;
 
-const textArea = document.getElementById("text");
-const errorElement = document.getElementById("error");
-const resultElement = document.getElementById("result");
-const codeButton = document.getElementById("code");
-const buferButton = document.getElementById("bufer");
+const textArea = document.getElementById('text');
+const errorElement = document.getElementById('error');
+const resultElement = document.getElementById('result');
+const codeButton = document.getElementById('code');
+const buferButton = document.getElementById('bufer');
 
 let reg = /^[a-zA-Z]+$/;
 
-let result = "";
+let result = '';
 
-const stringCoding = (str) => {
-  let uniqueSymbol = str[0];
-  let iterator = 0;
-  let resString = "";
+const stringCoding = str => {
+	let uniqueSymbol = str[0];
+	let iterator = 0;
+	let resString = '';
 
-  for (let i = 0; i <= str.length; i++) {
-    if (str[i] === uniqueSymbol) {
-      iterator = iterator + 1;
-    } else {
-      iterator === 1
-        ? (resString = resString + uniqueSymbol)
-        : (resString = resString + String(iterator) + uniqueSymbol);
+	for (let i = 0; i <= str.length; i++) {
+		if (str[i] === uniqueSymbol) {
+			iterator = iterator + 1;
+		} else {
+			iterator === 1
+				? (resString = resString + uniqueSymbol)
+				: (resString = resString + String(iterator) + uniqueSymbol);
 
-      uniqueSymbol = str[i];
-      iterator = 1;
-    }
-  }
+			uniqueSymbol = str[i];
+			iterator = 1;
+		}
+	}
 
-  return resString;
+	return resString;
 };
 
 const handleInput = () => {
-  if (!textArea.value) {
-    codeButton.setAttribute("disabled", true);
-    buferButton.setAttribute("disabled", true);
-    resultElement.innerHTML = "";
-    errorElement.innerHTML = "";
-    navigator.clipboard.writeText("");
-  } else if (!textArea.value.match(reg)) {
-    errorElement.innerHTML = "must be in english";
-    codeButton.setAttribute("disabled", true);
-    buferButton.setAttribute("disabled", true);
-    resultElement.innerHTML = "";
-  } else {
-    textArea.value = textArea.value.toUpperCase();
-    codeButton.removeAttribute("disabled");
-  }
+	if (!textArea.value) {
+		codeButton.setAttribute('disabled', true);
+		buferButton.setAttribute('disabled', true);
+		resultElement.innerHTML = '';
+		errorElement.innerHTML = '';
+		navigator.clipboard.writeText('');
+	} else if (!textArea.value.match(reg)) {
+		errorElement.innerHTML = 'must be in english';
+		codeButton.setAttribute('disabled', true);
+		buferButton.setAttribute('disabled', true);
+		resultElement.innerHTML = '';
+	} else {
+		textArea.value = textArea.value.toUpperCase();
+		codeButton.removeAttribute('disabled');
+		errorElement.innerHTML = '';
+	}
 };
 
 const handleCode = () => {
-  const stringForCoding = textArea.value;
-  resultElement.innerHTML = "";
-  result = stringCoding(stringForCoding);
-  resultElement.insertAdjacentHTML("afterbegin", result);
-  buferButton.removeAttribute("disabled");
+	const stringForCoding = textArea.value;
+	resultElement.innerHTML = '';
+	result = stringCoding(stringForCoding);
+	resultElement.insertAdjacentHTML('afterbegin', result);
+	buferButton.removeAttribute('disabled');
 };
 
 const handleCopyToBufer = () => {
-  navigator.clipboard.writeText(result);
+	navigator.clipboard.writeText(result);
 };
 
-textArea.addEventListener("input", handleInput);
-codeButton.addEventListener("click", handleCode);
-buferButton.addEventListener("click", handleCopyToBufer);
+textArea.addEventListener('input', handleInput);
+codeButton.addEventListener('click', handleCode);
+buferButton.addEventListener('click', handleCopyToBufer);
